62d07ee3bfad3eb55d733503b8aeafdc090ae10a
Jehiah Czebotar <jehiah@gmail.com>
2025-10-01 16:05:49 -0400

    import generated client

---
+++ src/apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BSDsApi';
+export * from './BitlinksApi';
+export * from './CampaignsApi';
+export * from './CustomBitlinksApi';
+export * from './DefaultApi';
+export * from './GroupsApi';
+export * from './OrganizationsApi';
+export * from './QRCodesApi';
+export * from './UserApi';
+export * from './WebhooksApi';
